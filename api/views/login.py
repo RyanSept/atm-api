506056daf82c71caeac4a8e2a9887c938a74ac9a
Ryan Marvin <marvinryan@ymail.com>
2018-01-10 23:02:06 +0300

    Change validation on login to verify pin hash

--- api/views/login.py
+++ api/views/login.py
@@ -5,6 +5,8 @@ from flask_restful import reqparse, Resource
 from api import db
 from api.models.account import Account
 from flask import current_app
+from passlib.hash import pbkdf2_sha256
+
 
 import datetime
 import jwt
@@ -15,17 +17,24 @@ class Login(Resource):
         # parse request json
         parser = reqparse.RequestParser()
         parser.add_argument("account_number", location="json", required=True)
-        parser.add_argument("pin", location="json", type=int, required=True)
+        parser.add_argument("pin", location="json", required=True)
         request_json = parser.parse_args()
         account = db.session.query(Account.id,
                                    Account.pin).filter_by(
             account_number=request_json.account_number).first()
 
-        if account is not None and account.pin == int(request_json["pin"]):
+        if account is None:
+            return {"message": "Invalid account number."}, 401
+
+        try:
+            pin_is_valid = pbkdf2_sha256.verify(request_json["pin"],
+                                                account.pin)
+            if not pin_is_valid:
+                return {"message": "Pin is invalid."}
+
             token = jwt.encode(
                 {
                     "account_id": account.id,
-                    "user_key": acc,
                     "iat": datetime.datetime.utcnow(),
                     "exp": datetime.datetime.utcnow() +
                     datetime.timedelta(
@@ -33,5 +42,8 @@ class Login(Resource):
                 }, current_app.config["SECRET_KEY"], algorithm='HS256')
 
             return {"token": token.decode("utf-8")}
-
-        return {"message": "Invalid pin."}, 401
+        except ValueError as error:
+            if "not a valid pbkdf2_sha256 hash" in str(error):
+                print("Invalid hash stored as pin.")
+            print(error)
+            return {"message": "Something went wrong."}, 500
