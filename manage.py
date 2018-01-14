050c19fa55f093c8bc53c99707128429cca4401c
Ryan Marvin <marvinryan@ymail.com>
2018-01-14 20:46:28 +0300

    Add app secret key generation command

--- manage.py
+++ manage.py
@@ -3,6 +3,9 @@ from api import create_app, db
 from api.models.account import Account
 from api.models.transaction import Transaction
 from api.config import load_config
+from binascii import hexlify
+
+import os
 
 app = create_app(load_config())
 manager = Manager(app)
@@ -14,6 +17,14 @@ def init_app():
     Initialise the app
     """
     db.create_all()
+
+
+@manager.command
+def generate_secret_key():
+    """
+    Generate random 1024 bit long hex for use as app secret
+    """
+    return "SECRET_KEY: " + hexlify(os.urandom(64)).decode("utf-8")
 
 
 if __name__ == '__main__':
